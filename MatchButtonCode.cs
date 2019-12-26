e51c3caad5cd8566f524e49b3d572bfe88a67933
DuvivierBri <47341822+duvivierbri@users.noreply.github.com>
2019-12-26 14:14:25 -0500

    Add files via upload

---
+++ MatchButtonCode.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MatchButtonCode : MonoBehaviour
+{
+    public void startTextScene(){
+        SceneManager.LoadScene("TextingScene");
+    }
+}
